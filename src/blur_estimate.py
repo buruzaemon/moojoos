bc111b682e242251b2a13c59e7ae8f2261f1e3ca
Brooke M. Fujita <buruzaemon@gmail.com>
2013-08-25 22:57:25 +0900

    WIP

--- src/blur_estimate.py
+++ src/blur_estimate.py
@@ -5,20 +5,20 @@ from pylab import *
 from scipy.ndimage import filters
 import os
 
+"""
 cd = os.path.dirname(os.path.abspath(__file__))
 fn = "800px-Monticello_2010-10-29.jpg"
 fp = os.path.join(cd, "data", fn)
 
 im = array(Image.open(fp).convert('L'))
+"""
 
-"""
-figure()
-gray()
-imshow(im)
-"""
+im = array(Image.open("C:/DEV/Area52/home/joker/dev/moojoos/test.jpg").convert("L"))
+
 figure()
 gray()
 imx = zeros(im.shape)
 filters.sobel(im, 1, imx)
 imshow(imx)
+plot(range(len(im[255])), im[255])
 show()
