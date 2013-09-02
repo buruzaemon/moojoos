c17c85dc648043423466cbd66a4035197660e5eb
Brooke M. Fujita <buruzaemon@gmail.com>
2013-09-02 21:32:22 +0900

    Adding script for creating test sample images, and the images themselves

--- src/blur_estimate.py
+++ src/blur_estimate.py
@@ -5,7 +5,6 @@ from pylab import *
 from scipy.ndimage import filters
 import os
 
-
 cd = os.path.dirname(os.path.abspath(__file__))
 fn = "test.jpg"
 fp = os.path.join(cd, fn)
@@ -26,23 +25,29 @@ gray()
 
 imx2 = zeros(imr.shape)
 filters.sobel(img2, 1, imx2)
-#imshow(imx2)
-subplot(131)
+subplot(231)
+imshow(imx2) 
+subplot(234)
 title(r'$\sigma=2$')
 plot(range(len(img2[r])), img2[r])
+plot(range(len(imx2[r])), imx2[r]) 
 
 imx3 = zeros(imr.shape)
 filters.sobel(img3, 1, imx3)
-#imshow(imx3)
-subplot(132)
+subplot(232)
+imshow(imx3)
+subplot(235)
 title(r'$\sigma=3$')
 plot(range(len(img3[r])), img3[r])
+plot(range(len(imx3[r])), imx3[r])
 
 imx4 = zeros(imr.shape)
 filters.sobel(img4, 1, imx4)
-#imshow(imx4)
-subplot(133)
+subplot(233)
+imshow(imx4)
+subplot(236)
 title(r'$\sigma=4$')
 plot(range(len(img4[r])), img4[r])
+plot(range(len(imx4[r])), imx4[r])
 
 show()
---
+++ src/create_testimages.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import os
+
+from PIL import Image
+from scipy import misc
+from scipy.ndimage import filters
+
+cd = os.path.dirname(os.path.abspath(__file__))
+fn = "test.jpg"
+fp = os.path.join(cd, fn)
+
+# transform raw image into grayscale
+img = Image.open(fp).convert('L')
+
+# next, create 3 copies of varying blurriness (Gaussian)
+blur = [2, 5, 10]
+for b in blur:
+    gd = filters.gaussian_filter(img, b)
+    misc.imsave("test_%s.jpg"%b, gd)
