66ca7dfd0849cc0b91f4ea32762d6f07f4e3084d
Brooke M. Fujita <buruzaemon@gmail.com>
2013-09-26 05:52:36 +0900

    ratios?

--- src/blur_estimate4.py
+++ src/blur_estimate4.py
@@ -8,13 +8,13 @@ from PIL import Image
 from scipy.ndimage import filters
 
 # target files for comparison
-files = [ 'rena_sharp.jpg', 'rena_gaussian_2.jpg' ]
+files = [ 'rena_sharp.jpg', 'rena_gaussian_10.jpg' ]
 
 cd = os.path.dirname(os.path.abspath(__file__))
 
 # gray-scaled images
 test1 = np.array(Image.open(os.path.join(cd, files[0])).convert('L'))
-test2 = np.array(Image.open(os.path.join(cd, files[1])).convert('L'))
+#test1 = np.array(Image.open(os.path.join(cd, files[1])).convert('L'))
 
 ## targetting rena's lips?
 #r = 260
@@ -22,15 +22,20 @@ test2 = np.array(Image.open(os.path.join(cd, files[1])).convert('L'))
 t1_sob = np.zeros(test1.shape)
 filters.sobel(test1,  1, t1_sob) 
 
-figure()
-gray()
+#igure()
+#gray()
 
-#for r in xrange(test1.shape[0]):
-for r in [260]:
+bhr = []
+
+for r in xrange(test1.shape[0]):
+#for r in [260]:
     t1s_mx, t1s_mn = mj.signal.peak_detect(t1_sob[r], lookahead=1, minpeak=4.0)
     t1s_peaks  = list(set(t1s_mx[0]+t1s_mn[0]))
     
     for p in t1s_peaks:
         s,e = mj.signal.find_edge_startend(t1_sob[r], p)        
-        plot(e-s, np.abs(p), 'g+')
-show()
+        #plot(e-s, np.abs(p), 'g+')
+        bhr.append(p/np.float(e-s))
+bins = np.arange(0, max(bhr))        
+hist(bhr, bins, normed=1, cumulative=1)    
+#show()
